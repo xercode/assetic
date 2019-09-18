ad5634c7d705b39d363fa6cd2e17e383ed3cfb0a
Xabier Fernández <javier.fernandez@xercode.es>
2019-09-18 09:05:12 +0200

    Using the "Twig_Extension_GlobalsInterface" class is deprecated since Twig version 2.7, use "Twig\Extension\GlobalsInterface"

--- src/Assetic/Extension/Twig/AsseticExtension.php
+++ src/Assetic/Extension/Twig/AsseticExtension.php
@@ -14,7 +14,7 @@ namespace Assetic\Extension\Twig;
 use Assetic\Factory\AssetFactory;
 use Assetic\ValueSupplierInterface;
 
-class AsseticExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+class AsseticExtension extends \Twig\Extension\AbstractExtension implements \Twig\Extension\GlobalsInterface
 {
     protected $factory;
     protected $functions;
